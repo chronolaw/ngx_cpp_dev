79a3e28f410e4b0c731cc7bc895b1d374359d590
Chrono Law <jianfeng.luo@chinacache.com>
2015-07-17 13:30:15 +0800

    config.sh

---
+++ config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+ngx_path=$HOME/nginx
+src_path=$HOME/ngx_cpp_dev
+
+cd $ngx_path
+
+./configure                                 \
+    --with-cc-opt="-g -O0"                  \
+    --add-module=$src_path/ngxpp            \
+    --add-module=$src_path/modules/test     \
+    --add-module=$src_path/modules/echo     \
+    --add-module=$src_path/modules/filter   \
+    --add-module=$src_path/modules/upstream \
+    --add-module=$src_path/modules/loadbalance \
+
+#    --with-ld-opt="-lrt"
+#    --add-module=$src_path/modules/datahook 
+#    --add-module=$src_path/modules/subrequest 
+#    --add-module=$src_path/modules/shmem    
+#    --add-module=$src_path/modules/variables
+
+cd -
