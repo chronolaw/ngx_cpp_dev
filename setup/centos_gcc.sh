c9302a2a082e0a8e02925ebbd1895a26721c4db3
Chrono Law <jianfeng.luo@chinacache.com>
2015-07-17 09:58:29 +0800

    add make and gcc shell

---
+++ setup/centos_gcc.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+GCC_V='4.7.3'
+sudo yum install -y glibc-static libstdc++-static
+
+wget http://ftp.gnu.org/gnu/gcc/gcc-${GCC_V}/gcc-${GCC_V}.tar.gz -O gcc-${GCC_V}.tar.gz
+
+tar xzf gcc-${GCC_V}.tar.gz
+cd gcc-${GCC_V}
+./contrib/download_prerequisites
+
+cd ..
+mkdir build_gcc${GCC_V}
+cd build_gcc${GCC_V}
+../gcc-${GCC_V}/configure --prefix=/usr/ --enable-checking=release --enable-languages=c,c++ --disable-multilib
+
+make -j23
+sudo make install
+
+cd ..
+
+rm -rf build_gcc${GCC_V} gcc-${GCC_V} gcc-${GCC_V}.tar.gz
