355c364df969cc10d396676d210876a95e237385
Chrono Law <jianfeng.luo@chinacache.com>
2015-11-02 14:09:29 +0800

    NgxRequestFilter

--- ngxpp/NgxFilter.hpp
+++ ngxpp/NgxFilter.hpp
@@ -55,4 +55,46 @@ private:
         return next_filter;
     }
 };
+
+// now only request body filter
+template<typename Tag>
+class NgxRequestFilter final
+{
+public:
+    typedef NgxRequestFilter this_type;
+    typedef ngx_http_request_body_filter_pt body_filter_ptr;
+
+    NgxRequestFilter() = default;
+    ~NgxRequestFilter() = default;
+
+public:
+    static void init(body_filter_ptr body_filter = nullptr)
+    {
+        if(body_filter)
+        {
+            set(ngx_http_top_request_body_filter, body_filter);
+        }
+    }
+public:
+    static ngx_int_t next(ngx_http_request_t* r, ngx_chain_t *chain)
+    {
+        return next<body_filter_ptr>()(r, chain);
+    }
+private:
+    template<typename T>
+    static void set(T& top, T p)
+    {
+        next<T>() = top;
+        top = p;
+    }
+
+    template<typename T>
+    static T& next()
+    {
+        static T next_filter;
+
+        return next_filter;
+    }
+};
+
 #endif  //_NGX_FILTER_HPP
