88e2a7a19d94dd6248a14bbc9a6d9fe18e3dfd23
chronolaw <chrono_cpp@me.com>
2016-05-25 09:03:13 +0800

    NgxEvent.hpp

--- ngxpp/NgxEvent.hpp
+++ ngxpp/NgxEvent.hpp
@@ -4,8 +4,10 @@
 #define _NGX_EVENT_HPP
 
 //#include "Nginx.hpp"
+#include "NgxWrapper.hpp"
+#include "NgxException.hpp"
 
-class NgxEvent final : public NgxWrapper<ngx_event_t>
+class NgxEvent : public NgxWrapper<ngx_event_t>
 {
 public:
     typedef NgxWrapper<ngx_event_t> super_type;
@@ -48,6 +50,37 @@ public:
     {
         get()->handler = f;
     }
+
+    // ngx_nil = ngx_del_timer
+    void timeout(ngx_msec_t timer, bool reset = false) const
+    {
+        if(get()->timer_set)
+        {
+            if(NgxValue::invalid(timer))
+            {
+                ngx_del_timer(get());
+                return;
+            }
+
+            if(!reset)
+            {
+                return;
+            }
+        }
+
+        ngx_add_timer(get(), timer);
+    }
+
+public:
+    bool ready() const
+    {
+        return get()->ready;
+    }
+
+    bool expired() const
+    {
+        return get()->timedout;
+    }
 public:
     void process() const
     {
@@ -55,4 +88,63 @@ public:
     }
 };
 
+class NgxReadEvent final: public NgxEvent
+{
+public:
+    typedef NgxEvent super_type;
+    typedef NgxReadEvent this_type;
+public:
+    NgxReadEvent(ngx_event_t* ev) : super_type(ev)
+    {
+        assert(!ev->write);
+    }
+
+    NgxReadEvent(ngx_event_t& ev) : super_type(ev)
+    {}
+
+    ~NgxReadEvent() = default;
+public:
+    void wait(ngx_uint_t flags = 0) const
+    {
+        auto rc = ngx_handle_read_event(get(), flags);
+
+        NgxException::require(rc);
+    }
+
+    void wait_for(ngx_msec_t timer, bool reset = false, ngx_uint_t flags = 0) const
+    {
+        timeout(timer, reset);
+        wait(flags);
+    }
+};
+
+class NgxWriteEvent final: public NgxEvent
+{
+public:
+    typedef NgxEvent super_type;
+    typedef NgxWriteEvent this_type;
+public:
+    NgxWriteEvent(ngx_event_t* ev) : super_type(ev)
+    {
+        assert(ev->write);
+    }
+
+    NgxWriteEvent(ngx_event_t& ev) : super_type(ev)
+    {}
+
+    ~NgxWriteEvent() = default;
+public:
+    void wait(size_t lowat = 0) const
+    {
+        auto rc = ngx_handle_write_event(get(), lowat);
+
+        NgxException::require(rc);
+    }
+
+    void wait_for(ngx_msec_t timer, bool reset = false, size_t lowat = 0) const
+    {
+        timeout(timer, reset);
+        wait(lowat);
+    }
+};
 #endif  //_NGX_EVENT_HPP
