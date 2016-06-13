3fbc77d4f6e1b5e275d08e641a273fb171cd633a
chronolaw <chrono_cpp@me.com>
2016-06-13 12:28:39 +0800

    config.sh

--- config.sh
+++ config.sh
@@ -4,22 +4,29 @@ ngx_path=$HOME/nginx
 src_path=$HOME/ngx_cpp_dev
 
 build_date="build at `date '+%Y.%m.%d %H:%M'`"
+cc_opt="-g -O0"
 
+ngxpp_module="--add-module=${src_path}/ngxpp"
+
+http_modules="--add-module=${src_path}/modules/test     \
+              --add-module=${src_path}/modules/echo     \
+              --add-module=${src_path}/modules/datahook \
+              --add-module=${src_path}/modules/filter   \
+              --add-module=${src_path}/modules/upstream \
+              --add-module=${src_path}/modules/loadbalance \
+              --add-module=${src_path}/modules/subrequest \
+              --add-module=${src_path}/modules/shmem    \
+              --add-module=${src_path}/modules/variables
+             "
+opts="${ngxpp_module}     \
+      ${http_modules}
+      "
 cd $ngx_path
 
-./configure                                 \
-    --build="$build_date"                   \
-    --with-cc-opt="-g -O0"                  \
-    --add-module=$src_path/ngxpp            \
-    --add-module=$src_path/modules/test     \
-    --add-module=$src_path/modules/echo     \
-    --add-module=$src_path/modules/datahook \
-    --add-module=$src_path/modules/filter   \
-    --add-module=$src_path/modules/upstream \
-    --add-module=$src_path/modules/loadbalance \
-    --add-module=$src_path/modules/subrequest \
-    --add-module=$src_path/modules/shmem    \
-    --add-module=$src_path/modules/variables
+./configure                     \
+    --build="${build_date}"     \
+    --with-cc-opt="${cc_opt}"   \
+    ${opts}
 
 #    --with-ld-opt="-lrt"
 
