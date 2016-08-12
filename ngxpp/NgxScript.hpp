37b1c0777415fd9d22ece155c4cc812596a0da5d
chronolaw <chrono_cpp@me.com>
2016-08-12 10:08:17 +0800

    NgxScript.hpp

--- ngxpp/NgxScript.hpp
+++ ngxpp/NgxScript.hpp
@@ -6,6 +6,28 @@
 #include "NgxException.hpp"
 #include "NgxValue.hpp"
 
+inline namespace ngx_script_types {
+#ifdef ngx_http_variable
+
+typedef ngx_http_request_t          ngx_script_ctx_t;
+typedef ngx_http_script_compile_t   ngx_script_compile_t;
+
+static auto ngx_script_count    = &ngx_http_script_variables_count;
+static auto ngx_script_compile  = &ngx_http_script_compile;
+static auto ngx_script_run     = &ngx_http_script_run;
+
+#else   //ngx_stream_variable
+
+typedef ngx_stream_session_t            ngx_script_ctx_t;
+typedef ngx_stream_script_compile_t     ngx_script_compile_t;
+
+static auto ngx_script_count    = &ngx_stream_script_variables_count;
+static auto ngx_script_compile  = &ngx_stream_script_compile;
+//static auto ngx_script_run     = &ngx_stream_script_run;
+
+#endif
+}
+
 class NgxScript final
 {
 public:
@@ -19,14 +41,14 @@ public:
 
         m_source = *source;
 
-        auto n = ngx_http_script_variables_count(source);
+        auto n = ngx_script_count(source);
 
         if(!n)      // n==0, no vars
         {
             return;
         }
 
-        ngx_http_script_compile_t sc;
+        ngx_script_compile_t sc;
         NgxValue::memzero(sc);
 
         sc.cf = cf;
@@ -37,7 +59,7 @@ public:
         sc.complete_lengths = true;
         sc.complete_values = true;
 
-        auto rc = ngx_http_script_compile(&sc);
+        auto rc = ngx_script_compile(&sc);
 
         NgxException::require(rc);
     }
@@ -52,16 +74,18 @@ public:
         return m_source;
     }
 
-    ngx_str_t str(ngx_http_request_t* r) const
+    ngx_str_t str(ngx_script_ctx_t* r) const
     {
         if(!m_lengths)  // raw string
         {
             return m_source;
         }
 
+        assert(m_values);
+
         ngx_str_t value;
 
-        auto pos = ngx_http_script_run(r, &value,
+        auto pos = ngx_script_run(r, &value,
                         m_lengths->elts, 0, m_values->elts);
 
         NgxException::require(pos);
