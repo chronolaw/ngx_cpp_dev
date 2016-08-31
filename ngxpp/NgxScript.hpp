55ac3355b8843e2c16503715fa9f77504a5d7cea
chronolaw <chrono_cpp@me.com>
2016-08-31 13:22:35 +0800

    NgxScript.hpp enabled in stream

--- ngxpp/NgxScript.hpp
+++ ngxpp/NgxScript.hpp
@@ -5,25 +5,28 @@
 
 #include "NgxException.hpp"
 #include "NgxValue.hpp"
+#include "NgxPool.hpp"
 
 inline namespace ngx_script_types {
 #ifdef ngx_http_variable
 
-typedef ngx_http_request_t          ngx_script_ctx_t;
-typedef ngx_http_script_compile_t   ngx_script_compile_t;
+typedef ngx_http_request_t                  ngx_script_ctx_t;
 
-static auto ngx_script_count    = &ngx_http_script_variables_count;
-static auto ngx_script_compile  = &ngx_http_script_compile;
-static auto ngx_script_run     = &ngx_http_script_run;
+typedef ngx_http_compile_complex_value_t    ngx_compile_complex_value_t;
+typedef ngx_http_complex_value_t            ngx_complex_value_t;
+
+static auto ngx_compile_complex_value       = &ngx_http_compile_complex_value;
+static auto ngx_complex_value               = &ngx_http_complex_value;
 
 #else   //ngx_stream_variable
 
-typedef ngx_stream_session_t            ngx_script_ctx_t;
-typedef ngx_stream_script_compile_t     ngx_script_compile_t;
+typedef ngx_stream_session_t                ngx_script_ctx_t;
 
-static auto ngx_script_count    = &ngx_stream_script_variables_count;
-static auto ngx_script_compile  = &ngx_stream_script_compile;
-//static auto ngx_script_run     = &ngx_stream_script_run;
+typedef ngx_stream_compile_complex_value_t  ngx_compile_complex_value_t;
+typedef ngx_stream_complex_value_t          ngx_complex_value_t;
+
+static auto ngx_compile_complex_value       = &ngx_stream_compile_complex_value;
+static auto ngx_complex_value               = &ngx_stream_complex_value;
 
 #endif
 }
@@ -41,27 +44,27 @@ public:
 
         m_source = *source;
 
-        auto n = ngx_script_count(source);
+        ngx_complex_value_t         cv;
+        ngx_compile_complex_value_t ccv;
 
-        if(!n)      // n==0, no vars
+        NgxValue::memzero(ccv);
+
+        ccv.cf = cf;
+        ccv.value = source;
+        ccv.complex_value = &cv;
+
+        auto rc = ngx_compile_complex_value(&ccv);
+
+        NgxException::require(rc);
+
+        if(!cv.lengths)   // raw string
         {
             return;
         }
 
-        ngx_script_compile_t sc;
-        NgxValue::memzero(sc);
+        m_cv = NgxPool(cf).alloc<ngx_complex_value_t>();
 
-        sc.cf = cf;
-        sc.source = &m_source;
-        sc.lengths = &m_lengths;
-        sc.values = &m_values;
-        sc.variables = n;
-        sc.complete_lengths = true;
-        sc.complete_values = true;
-
-        auto rc = ngx_script_compile(&sc);
-
-        NgxException::require(rc);
+        *m_cv = cv;
     }
 
     void init(ngx_conf_t* cf, ngx_str_t& source)
@@ -76,26 +79,23 @@ public:
 
     ngx_str_t str(ngx_script_ctx_t* r) const
     {
-        if(!m_lengths)  // raw string
+        if(!m_cv)   // raw string
         {
             return m_source;
         }
 
-        assert(m_values);
+        ngx_str_t value = ngx_null_string;
 
-        ngx_str_t value;
+        auto rc = ngx_complex_value(r, m_cv, &value);
 
-        auto pos = ngx_script_run(r, &value,
-                        m_lengths->elts, 0, m_values->elts);
-
-        NgxException::require(pos);
+        NgxException::require(rc);
 
         return value;
     }
 private:
-    ngx_str_t       m_source = ngx_null_string;
-    ngx_array_t*    m_lengths = nullptr;
-    ngx_array_t*    m_values = nullptr;
+    ngx_str_t           m_source = ngx_null_string;
+
+    ngx_complex_value_t *m_cv = nullptr;
 };
 
 #endif  //_NGX_SCRIPT_HPP
