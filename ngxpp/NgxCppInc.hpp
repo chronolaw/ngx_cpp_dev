ed8fa1e234f37a3b603e5a73bcc583f6670ee549
chronolaw <chrono_cpp@me.com>
2016-09-01 10:36:30 +0800

    NgxCppInc.hpp

---
+++ ngxpp/NgxCppInc.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2016
+// Author: Chrono Law
+#ifndef _NGX_CPPINC_HPP
+#define _NGX_CPPINC_HPP
+
+#include <cassert>
+
+#include <string>
+#include <deque>
+#include <exception>
+
+#include <boost/core/ignore_unused.hpp>
+#include <boost/core/explicit_operator_bool.hpp>
+
+#include <boost/format.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/next_prior.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+
+#include <boost/functional/factory.hpp>
+#include <boost/exception/all.hpp>
+#include <boost/iterator/iterator_traits.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+#include <boost/iterator/reverse_iterator.hpp>
+#include <boost/tti/member_type.hpp>
+#include <boost/tti/has_member_data.hpp>
+#include <boost/utility/enable_if.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/mpl/logical.hpp>
+
+#endif  // _NGX_CPPINC_HPP
