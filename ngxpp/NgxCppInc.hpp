30d32f341fe14df3e110793640268f2c02dd3b40
chronolaw <chrono_cpp@me.com>
2016-09-07 10:08:42 +0800

    NgxCppInc.hpp

--- ngxpp/NgxCppInc.hpp
+++ ngxpp/NgxCppInc.hpp
@@ -12,23 +12,25 @@
 #include <boost/core/ignore_unused.hpp>
 #include <boost/core/explicit_operator_bool.hpp>
 
-#include <boost/format.hpp>
-#include <boost/static_assert.hpp>
-#include <boost/next_prior.hpp>
 #include <boost/type_traits.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/next_prior.hpp>
+#include <boost/static_assert.hpp>
 #include <boost/function.hpp>
+//#include <boost/format.hpp>
 
-#include <boost/functional/factory.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/exception/all.hpp>
+#include <boost/functional/factory.hpp>
 #include <boost/iterator/iterator_traits.hpp>
 #include <boost/iterator/iterator_facade.hpp>
 #include <boost/iterator/reverse_iterator.hpp>
+#include <boost/mpl/logical.hpp>
+#include <boost/mpl/map.hpp>
+#include <boost/mpl/at.hpp>
 #include <boost/tti/member_type.hpp>
 #include <boost/tti/has_member_data.hpp>
 #include <boost/utility/enable_if.hpp>
 #include <boost/utility/string_ref.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/mpl/logical.hpp>
 
 #endif  // _NGX_CPPINC_HPP
