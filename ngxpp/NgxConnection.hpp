07c7ad7f94a501b684dcb4c11a412bd485035e66
chronolaw <chrono_cpp@me.com>
2017-04-03 14:49:14 +0800

    NgxConnection.hpp

--- ngxpp/NgxConnection.hpp
+++ ngxpp/NgxConnection.hpp
@@ -3,9 +3,9 @@
 #ifndef _NGX_CONNECTION_HPP
 #define _NGX_CONNECTION_HPP
 
-#include <boost/type_traits.hpp>
-#include <boost/tti/member_type.hpp>
-#include <boost/utility/enable_if.hpp>
+//#include <boost/type_traits.hpp>
+//#include <boost/tti/member_type.hpp>
+//#include <boost/utility/enable_if.hpp>
 
 #include "NgxWrapper.hpp"
 #include "NgxException.hpp"
